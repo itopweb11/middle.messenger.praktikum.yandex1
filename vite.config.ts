4e606ceb684bdc0134df9455703ecf1c6aca7b62
itopweb11 <68381487+itopweb11@users.noreply.github.com>
2024-07-14 00:07:19 +0300

    Исправление отоюражения страницы регистрации

--- vite.config.ts
+++ vite.config.ts
@@ -14,6 +14,7 @@ export default defineConfig({
         rollupOptions: {
             input: {
                 main: resolve(root, 'index.html'),
+                registration: resolve(root, 'registration.html'),
             }
         }
     }
