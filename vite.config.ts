1ab70d0ff2bf9d2682346de29cc4063c1968e970
itopweb11 <68381487+itopweb11@users.noreply.github.com>
2024-07-18 22:16:39 +0300

    Добавление страниц в vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
-import { resolve } from 'path';
-import { defineConfig } from 'vite'
+import {resolve} from 'path';
+import {defineConfig} from 'vite'
 import handlebars from 'vite-plugin-handlebars';
 
 const root = resolve(__dirname, 'src/pages')
@@ -14,7 +14,11 @@ export default defineConfig({
         rollupOptions: {
             input: {
                 main: resolve(root, 'index.html'),
+                404: resolve(root, '404.html'),
+                500: resolve(root, '500.html'),
                 authorization: resolve(root, 'authorization.html'),
+                chat: resolve(root, 'chat.html'),
+                profile: resolve(root, 'profile.html'),
                 registration: resolve(root, 'registration.html'),
             }
         }
